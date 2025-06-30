0f32a737a6ad182151e45c225adb41822cceaf6b
Sumit Mahadik <160775655+smahadik0206@users.noreply.github.com>
2025-06-30 17:57:13 +0530

    Update deployment.py

---
+++ src/mcpserver/deployment.py
@@ -0,0 +1,34 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+
+
+mcp = FastMCP('WeatherApp')
+API_KEY = ''
+
+@mcp.tool()
+def get_weather(location:str) -> str:
+    """
+    Gets the weather given a location
+    Args:
+        location: location, can be city, state, country etc.
+    """
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}&units=metric"
+    
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code != 200:
+            return f"Error: {data.get('message', 'Failed to fetch weather')}"
+
+        temp = data["main"]["temp"]
+        description = data["weather"][0]["description"]
+        return f"The weather in {location} is {description} with a temperature of {temp}°C"
+
+    except Exception as e:
+        return f"Error retrieving weather: {str(e)}"
+
+if __name__ == "__main__":
+    mcp.run()
+    
